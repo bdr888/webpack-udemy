139db68c6be3c8298ebb26380b15e435408befac
Brian Ridge <brian.ridge@dish.com>
2018-07-31 16:05:36 -0600

    feat(client): add babel stuff

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,14 @@ const config = {
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        use: 'babel-loader',
+        test: /\.js$/
+      }
+    ]
   }
 };
 
