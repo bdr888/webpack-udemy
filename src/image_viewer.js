838702b078eeb6fcf21757a3f57bf810794660e4
briandridge <briandridge@gmail.com>
2018-08-04 11:07:15 -0600

    feat(client): code splitting

--- src/image_viewer.js
+++ src/image_viewer.js
@@ -1,14 +1,10 @@
-import './image_viewer.css';
-import big from '../assets/big.jpg';
 import small from '../assets/small.jpg'
+import './image_viewer.css';
 
-const image = document.createElement('img');
-image.src = small;
+export default () => {
+  const image = document.createElement('img');
+  image.src = small;
 
-document.body.appendChild(image);
+  document.body.appendChild(image);
+};
 
-
-const bigImage = document.createElement('img');
-bigImage.src = big;
-
-document.body.appendChild(bigImage);
--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
-import sum from './sum';
-import './image_viewer';
+const button = document.createElement('button');
+button.innerText = 'Click Me!!!';
 
-const total = sum(10,5);
-console.log(total);
+button.onclick = () => {
+  System.import('./image_viewer').then(module => {
+    module.default(); 
+  });
+};
+
+document.body.appendChild(button);
+
