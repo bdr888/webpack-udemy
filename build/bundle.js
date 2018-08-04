14f0a19f8437d392ae3af86ac527d6102960620c
briandridge <briandridge@gmail.com>
2018-08-04 10:21:57 -0600

    feat(client): makes images work

--- build/bundle.js
+++ build/bundle.js
@@ -60,7 +60,7 @@
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "";
+/******/ 	__webpack_require__.p = "build/";
 /******/
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(__webpack_require__.s = 5);
@@ -86,9 +86,14 @@ var _small2 = _interopRequireDefault(_small);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var image = document.createElement('img');
-image.src = 'http://lorempixel.com/400/400';
+image.src = _small2.default;
 
 document.body.appendChild(image);
+
+var bigImage = document.createElement('img');
+bigImage.src = _big2.default;
+
+document.body.appendChild(bigImage);
 
 /***/ },
 /* 1 */
--- src/image_viewer.js
+++ src/image_viewer.js
@@ -3,7 +3,12 @@ import big from '../assets/big.jpg';
 import small from '../assets/small.jpg'
 
 const image = document.createElement('img');
-image.src = 'http://lorempixel.com/400/400';
+image.src = small;
 
 document.body.appendChild(image);
 
+
+const bigImage = document.createElement('img');
+bigImage.src = big;
+
+document.body.appendChild(bigImage);
